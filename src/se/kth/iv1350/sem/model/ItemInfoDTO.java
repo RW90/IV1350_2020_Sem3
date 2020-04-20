799b23c0307630694b56d2998ed551f8d8cc6cba
RillMeister <riwa@kth.se>
2020-04-20 13:05:41 +0200

    Added DiscountDTO class

--- src/se/kth/iv1350/sem/model/ItemInfoDTO.java
+++ src/se/kth/iv1350/sem/model/ItemInfoDTO.java
@@ -9,6 +9,7 @@ public class ItemInfoDTO {
     private  final String description;
     private final int id;
     private final String name;
+    private final DiscountDTO discount;
 
     /**
      * Creates a new immutable instance of ItemInfoDTO, representing data of an unique item.
@@ -18,12 +19,13 @@ public class ItemInfoDTO {
      * @param id Unique id-number of item.
      * @param name Item name.
      */
-    public ItemInfoDTO(int price, float VATRate, String description, int id, String name) {
+    public ItemInfoDTO(int price, float VATRate, String description, int id, String name, DiscountDTO discount) {
         this.price = price;
         this.VATRate = VATRate;
         this.description = description;
         this.id = id;
         this.name = name;
+        this.discount = discount;
     }
 
     /**
@@ -64,5 +66,13 @@ public class ItemInfoDTO {
      */
     public String getName() {
         return name;
+    }
+
+    /**
+     * Get item discount.
+     * @return Item discount.
+     */
+    public DiscountDTO getDiscount() {
+        return this.discount;
     }
 }
