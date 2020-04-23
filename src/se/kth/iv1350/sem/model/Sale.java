fddcf026e7273434c260290ae4614227128d934f
RillMeister <riwa@kth.se>
2020-04-23 12:30:46 +0200

    Sale addItem klar, oklara getters i sale

--- src/se/kth/iv1350/sem/model/Sale.java
+++ src/se/kth/iv1350/sem/model/Sale.java
@@ -24,22 +24,64 @@ public class Sale {
         this.customerDiscount = null;
     }
 
+    public int getTotalVAT() {
+        return totalVAT;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public DiscountDTO getCustomerDiscount() {
+        return customerDiscount;
+    }
+
     /**
      * Add an item to the current sale and increment price accordingly.
      * @param item Item to add, represented by an <code>ItemInfoDTO</code>.
      * @param amount The amount of specified item to add.
      */
     public void addItem(ItemInfoDTO item, int amount) {
-        //TODO: this
+        if(isItemInSale(item)) {
+            increaseItemAmount(item, amount);
+        } else {
+            addNewItem(item, amount);
+        }
+        increasePrice(item, amount);
+    }
+
+    private void increasePrice(ItemInfoDTO item, int amount) {
+        this.priceExcludingVAT += item.getPrice() * amount;
+        this.totalVAT += (int) (item.getPrice() * item.getVATRate() * amount);
+        this.totalPrice = this.priceExcludingVAT + this.totalVAT;
+    }
+    private void addNewItem(ItemInfoDTO item, int amount) {
+        Item newItem = new Item(amount, item);
+        items.add(newItem);
+    }
+    private void increaseItemAmount(ItemInfoDTO item, int amount) {
+        int itemIndex = findIndexOfItem(item);
+        Item itemToIncrement = items.get(itemIndex);
+        itemToIncrement.addAmount(amount);
     }
 
     private boolean isItemInSale(ItemInfoDTO searchedItem) {
+        return findIndexOfItem(searchedItem) != -1;
+    }
+
+    private int findIndexOfItem(ItemInfoDTO itemToFind) {
+        int index = 0;
         for(Item itemInSale : items) {
-            if (itemInSale.isItem(searchedItem)) {
-                return true;
+            if(itemInSale.isItem(itemToFind)) {
+                return index;
             }
+            index++;
         }
-        return false;
+        return -1;
     }
     
 
