8788999fd3b39383dbe73bfd216157845c62a896
RillMeister <riwa@kth.se>
2020-05-04 23:16:03 +0200

    Skapat stubs för klasser

---
+++ src/se/kth/iv1350/sem/model/DatabaseConnectionFailureException.java
@@ -0,0 +1,4 @@
+package se.kth.iv1350.sem.model;
+
+public class DatabaseConnectionFailureException extends RuntimeException {
+}
---
+++ src/se/kth/iv1350/sem/view/TotalRevenueView.java
@@ -0,0 +1,4 @@
+package se.kth.iv1350.sem.view;
+
+public class TotalRevenueView {
+}
